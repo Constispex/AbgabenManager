3be8c0fbe5d4e4c434c92ab88a4521b851933e82
Constispex <113670532+Constispex@users.noreply.github.com>
2023-08-30 08:24:23 +0200

    zum arbeiten in der Bib

--- app/src/main/java/de/medieninformatik/androidapp/views/AddFragment.kt
+++ app/src/main/java/de/medieninformatik/androidapp/views/AddFragment.kt
@@ -1,10 +1,18 @@
 package de.medieninformatik.androidapp.views
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
+import com.google.android.material.textfield.TextInputEditText
+import com.google.gson.Gson
+import de.medieninformatik.abgabenmanager.model.Abgabe
+import de.medieninformatik.abgabenmanager.model.Task
 import de.medieninformatik.androidapp.R
 
 class AddFragment : Fragment() {
@@ -13,7 +21,47 @@ class AddFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.add_fragment, container, false)
+        val view = inflater.inflate(
+            R.layout.add_fragment, container, false)
+
+        val in_name:TextInputEditText = view.findViewById(R.id.input_name)
+        val in_description:TextInputEditText = view.findViewById(R.id.input_description)
+        val in_date:EditText = view.findViewById(R.id.input_date)
+        val in_subject:TextInputEditText = view.findViewById(R.id.input_subject)
+        var tasks = listOf<Task>()
+
+        val button_add_task = view.findViewById(R.id.button_createToDos) as Button
+        val button_submit = view.findViewById(R.id.button_submit) as Button
+
+        button_add_task.setOnClickListener {
+        // TODO: Add tasks to list
+        }
+        button_submit.setOnClickListener {
+            val abgabe:Abgabe = Abgabe(getLastId(), in_name.text.toString(), in_date.text.toString(), in_subject.text.toString(), tasks, in_description.text.toString(), false)
+            Log.i("Abgabe", abgabe.toString())
+            addToJson(abgabe)
+
+        }
+
+
+        return view
     }
 
+    private fun getLastId(): Int {
+        return 1;
+    }
+
+    private fun addToJson(abgabe: Abgabe) {
+        val gson = Gson()
+        val newJson = gson.toJson(abgabe)
+        val sharedPreferences = context?.getSharedPreferences("sharedPrefs", 0)
+        val editor = sharedPreferences?.edit()
+        var savedJson = sharedPreferences?.getString("json", "")
+
+        savedJson += newJson
+
+        editor?.putString("json", savedJson)
+        editor?.apply()
+        Log.i("json-content", sharedPreferences?.getString("json", "").toString())
+    }
 }
--- app/src/main/java/de/medieninformatik/androidapp/views/HomeFragment.kt
+++ app/src/main/java/de/medieninformatik/androidapp/views/HomeFragment.kt
@@ -1,5 +1,6 @@
 package de.medieninformatik.androidapp.views
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -7,9 +8,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ListView
 import androidx.fragment.app.Fragment
+import com.google.gson.Gson
 import de.medieninformatik.abgabenmanager.model.Abgabe
 import de.medieninformatik.androidapp.R
 import de.medieninformatik.androidapp.model.AbgabeAdapter
+import org.json.JSONObject
+import java.io.FileReader
 
 class HomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -25,11 +29,31 @@ class HomeFragment : Fragment() {
 
     private fun getAbgaben(): MutableList<Abgabe> {
 
-            val abgaben = mutableListOf<Abgabe>()
-            abgaben.add(Abgabe(1, "Abgabe 1", "01.01.2021", "MCI", listOf(), "Beschreibung 1", false))
-            abgaben.add(Abgabe(2, "Abgabe 2", "02.01.2021", "MCI", listOf(), "Beschreibung 2", false))
-            abgaben.add(Abgabe(3, "Abgabe 3", "03.01.2021", "MCI", listOf(), "Beschreibung 3", false))
+        val abgaben = mutableListOf<Abgabe>()
+        abgaben.add(Abgabe(1, "Abgabe 1", "01.01.2021", "MCI", listOf(), "Beschreibung 1", false))
+        abgaben.add(Abgabe(2, "Abgabe 2", "02.01.2021", "MCI", listOf(), "Beschreibung 2", false))
+        abgaben.add(Abgabe(3, "Abgabe 33", "03.01.2021", "MCI", listOf(), "Beschreibung 3", false))
 
-            return abgaben
+        val sharedPreferences = context?.getSharedPreferences("json", Context.MODE_PRIVATE)
+        val gson = Gson()
+
+        val jsonString = sharedPreferences?.getString("json", "")
+
+        if (jsonString != "" && jsonString != null) {
+            val jsonObject = JSONObject(jsonString)
+            Log.d("JSON", jsonObject.toString())
+
+            val jsonLength= jsonObject.length()
+            for (i in 0 until jsonLength) {
+                val abgabe = gson.fromJson(jsonObject.getJSONObject(i.toString()).toString(), Abgabe::class.java)
+                abgaben.add(abgabe)
+            }
+
+        } else {
+            Log.d("JSON", "No JSON found")
+        }
+
+
+        return abgaben
     }
 }
